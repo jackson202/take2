5f2154649ad72cdb6486e4a130380ff13a34be4d
insert <jackson14@live.com.au>
2019-05-30 03:48:20 +1000

    Added user story 6

--- AFLSite/src/app/app.component.ts
+++ AFLSite/src/app/app.component.ts
@@ -5,12 +5,36 @@ import { Component, EventEmitter, Output } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+     // General
+
 export class AppComponent {
   title = 'AFLSite';
 
+  
  show: boolean = true;
+ speak: boolean = false;
 
 showComponent(){
   this.show = !this.show;
 }
+
+makeTrue()
+{
+  this.speak = true;
+}
+
+sayText()
+{
+  var speak = window.getSelection().toString(); 
+  if(this.speak)
+  {
+  var msg = new SpeechSynthesisUtterance(speak);
+  }
+ window.speechSynthesis.speak(msg);
+ speak = ""
+
+
+}
+
 }
